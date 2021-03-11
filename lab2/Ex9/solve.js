90ac874736348d4da9c6ecc7423e188f93412ef6
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-11 20:12:11 +0700

    no message

--- lab2/Ex9/solve.js
+++ lab2/Ex9/solve.js
@@ -41,54 +41,7 @@ function changecss(obj) {
             } else if (i == 1) {
                 css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="dark.css" id="css_path"/>'
             } else if (i == 2) {
-
-            }
-        }
-    }
-}
-
-function validateSelectBox(obj) {
-    var table = document.getElementById('table');
-    var list_btn = document.getElementsByTagName('input');
-    var body = document.getElementById('body');
-    var test_display = document.getElementById("txt_result");
-    var options = obj.children;
-    for (var i=0; i<options.length; i++) {
-        if(options[i].selected) {
-            if(i == 0) {
-                body.classList.remove('body_dark');
-                body.classList.remove('body_colorfull');
-                body.classList.add('body_default');
-                
-                table.classList.remove('table_dark');
-                table.classList.add('table_default');
-                
-                test_display.classList.remove('text_dark');
-                test_display.classList.remove('text_colorful');
-                test_display.classList.add('text_default');
-                for(var y=0; y<list_btn.length; y++) {
-                    list_btn[y].classList.remove('Dark');
-                    list_btn[y].classList.remove('Colorful');
-                    list_btn[y].classList.add('Default')
-                }
-            } else if (i == 1) {
-                body.classList.remove('body_default');
-                body.classList.remove('body_colorfull');
-                body.classList.add('body_dark');
-
-                table.classList.remove('table_default');
-                table.classList.add('table_dark');
-
-                test_display.classList.remove('text_default');
-                test_display.classList.remove('text_colorful');
-                test_display.classList.add('text_dark');
-                for(var y=0; y<list_btn.length; y++) {
-                    list_btn[y].classList.remove('Colorful');
-                    list_btn[y].classList.remove('Default')
-                    list_btn[y].classList.add('Dark');
-                }
-            } else if (i == 2) {
-                
+                css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="colorful.css" id="css_path"/>'
             }
         }
     }
