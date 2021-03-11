f08173c5abc81f017853c0de91e9b938ac869c29
HaoHuynh <hao152903@gmail.com>
2021-03-11 14:41:31 +0700

    Upload ex9

---
+++ lab2/Ex9/solve.js
@@ -0,0 +1,70 @@
+var flag = true;
+
+function display(num) {
+    var test_display = document.getElementById("txt_result");
+    if(flag == true) {
+        test_display.value = test_display.value + num;
+    }
+    else {
+        test_display.value = "";
+        test_display.value = test_display.value + num;
+        flag = true;
+    }
+}
+
+function clear_all() {
+    var test_display = document.getElementById("txt_result");
+    test_display.value = "";
+    console.log("Done");
+}
+
+function solve() {
+    flag = false;
+    var test_display = document.getElementById("txt_result");
+    try {
+        var result = eval(test_display.value);
+        test_display.value = result;
+    }
+    catch(exception) {
+        test_display.value = "";
+        alert(exception);
+    }
+}
+
+function validateSelectBox(obj) {
+    var table = document.getElementById('table');
+    var list_btn = document.getElementsByClassName('btn_change');
+    var test_display = document.getElementById("txt_result");
+    var options = obj.children;
+    for (var i=0; i<options.length; i++) {
+        if(options[i].selected) {
+            if(i == 0) {
+                table.classList.remove('table_dark');
+                table.classList.add('table_default');
+                
+                test_display.classList.remove('text_dark');
+                test_display.classList.remove('text_colorful');
+                test_display.classList.add('text_default');
+                for(var y=0; y<list_btn.length; y++) {
+                    list_btn[y].classList.remove('Dark');
+                    list_btn[y].classList.remove('Colorful');
+                    list_btn[y].classList.add('Default')
+                }
+            } else if (i == 1) {
+                table.classList.remove('table_default');
+                table.classList.add('table_dark');
+
+                test_display.classList.remove('text_default');
+                test_display.classList.remove('text_colorful');
+                test_display.classList.add('text_dark');
+                for(var y=0; y<list_btn.length; y++) {
+                    list_btn[y].classList.remove('Colorful');
+                    list_btn[y].classList.remove('Default')
+                    list_btn[y].classList.add('Dark');
+                }
+            } else if (i == 2) {
+                
+            }
+        }
+    }
+}
