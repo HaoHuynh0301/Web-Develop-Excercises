42ff5d95155f0a99f064c047c2e93cdfd6e9e33e
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-12 13:40:30 +0700

    no message

--- lab2/Ex9/solve.js
+++ lab2/Ex9/solve.js
@@ -1,48 +1,51 @@
-var flag = true;
+document.addEventListener("DOMContentLoaded", function () {
+    var flag = true;
 
-function display(num) {
-    var test_display = document.getElementById("txt_result");
-    if(flag == true) {
-        test_display.value = test_display.value + num;
+    function display(num) {
+        var test_display = document.getElementById("txt_result");
+        if(flag == true) {
+            test_display.value = test_display.value + num;
+        }
+        else {
+            test_display.value = "";
+            test_display.value = test_display.value + num;
+            flag = true;
+        }
     }
-    else {
+    
+    function clear_all() {
+        var test_display = document.getElementById("txt_result");
         test_display.value = "";
-        test_display.value = test_display.value + num;
-        flag = true;
+        console.log("Done");
     }
-}
-
-function clear_all() {
-    var test_display = document.getElementById("txt_result");
-    test_display.value = "";
-    console.log("Done");
-}
-
-function solve() {
-    flag = false;
-    var test_display = document.getElementById("txt_result");
-    try {
-        var result = eval(test_display.value);
-        test_display.value = result;
+    
+    function solve() {
+        flag = false;
+        var test_display = document.getElementById("txt_result");
+        try {
+            var result = eval(test_display.value);
+            test_display.value = result;
+        }
+        catch(exception) {
+            test_display.value = "";
+            alert(exception);
+        }
     }
-    catch(exception) {
-        test_display.value = "";
-        alert(exception);
-    }
-}
-
-function changecss(obj) {
-    var options = obj.children;
-    var css_path = document.getElementById('css_path');
-    for (var i=0; i<options.length; i++) {
-        if(options[i].selected) {
-            if(i == 0) {
-                css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="default.css" id="css_path"/>';
-            } else if (i == 1) {
-                css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="dark.css" id="css_path"/>'
-            } else if (i == 2) {
-                css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="colorful.css" id="css_path"/>'
+    
+    function changecss(obj) {
+        var options = obj.children;
+        var css_path = document.getElementById('css_path');
+        for (var i=0; i<options.length; i++) {
+            if(options[i].selected) {
+                if(i == 0) {
+                    css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="default.css" id="css_path"/>';
+                } else if (i == 1) {
+                    css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="dark.css" id="css_path"/>'
+                } else if (i == 2) {
+                    css_path.innerHTML = '<link type="text/css" rel="stylesheet" href="colorful.css" id="css_path"/>'
+                }
             }
         }
     }
-}
+    
+}, false)
