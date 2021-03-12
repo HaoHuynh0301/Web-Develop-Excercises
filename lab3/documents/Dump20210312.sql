ffafad0ff657138fbc18ee9ccbfb313a63228db1
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-12 18:33:37 +0700

    Update lab3's documents

---
+++ lab3/documents/Dump20210312.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: music
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `baiviet`
+--
+
+DROP TABLE IF EXISTS `baiviet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `baiviet` (
+  `ma_bviet` int unsigned NOT NULL DEFAULT (NULL),
+  `tieude` varchar(200) NOT NULL DEFAULT (NULL),
+  `ten_bhat` varchar(100) NOT NULL DEFAULT (NULL),
+  `ma_tloai` int unsigned NOT NULL DEFAULT (NULL),
+  `tomtat` varchar(500) NOT NULL DEFAULT (NULL),
+  `baiviet` varchar(2000) DEFAULT (NULL),
+  `ma_tgia` int unsigned NOT NULL DEFAULT (NULL),
+  `ngayviet` date NOT NULL DEFAULT (NULL),
+  `image` longblob DEFAULT (NULL),
+  PRIMARY KEY (`ma_bviet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `baiviet`
+--
+
+LOCK TABLES `baiviet` WRITE;
+/*!40000 ALTER TABLE `baiviet` DISABLE KEYS */;
+INSERT INTO `baiviet` VALUES (1,'Lòng mẹ','Lòng mẹ',2,'Và mẹ ơi đừng khóc nhé! Cả đời này mẹ đã khóc nhiều lắm rồi, hãy cười lên vì con đã trưởng thành! Con sẽ lại về dậy sớm nấu cơm cho mẹ, nấu nước cho mẹ tắm như ngày xưa. “Dù cho vai nắng nhưng lòng thương chẳng nhạt màu, vẫn mơ quay về vui vầy dưới bóng mẹ yêu”',NULL,1,'2012-07-23',NULL),(2,'Cảm ơn em đã rời xa anh','Vết mưa',2,'Cảm ơn em đã cho anh những tháng ngày hạnh phúc, cho anh biết yêu và được yêu. Em cho anh được nếm trải hương vị ngọt ngào của tình yêu nhưng cũng đầy đau khổ và nước mắt. Những tháng ngày đó có lẽ suốt cuộc đời anh không bao giờ quên',NULL,3,'2012-02-12',NULL),(3,'Cuộc đời có mấy ngày mai?','Phôi pha',2,'Đêm nay, trời quang mây tạnh, trong người nghe hoang vắng và tôi ngồi đây “Ôm lòng đêm, Nhìn vầng trăng mới về” mà ngậm ngùi “Nhớ chân giang hồ. Ôi phù du, từng tuổi xuân đã già”',NULL,4,'2014-03-13',NULL),(4,'Quê tôi!','Quê hương',5,'Quê hương là gì mà chở đầy kí ức nhỏ xinh. Có đám trẻ nô đùa bên nhau dưới gốc ổi nhà bà Năm giữa trưa nắng gắt chỉ để chờ bà đi vắng là hái trộm. Có hai anh em tôi bì bõm lội sình bắt cua đem về nhà cho mẹ nấu canh, nấu cháo… Có ba chị em tôi lục đục tự nấu ăn khi mẹ vắng nhà. Có anh tôi luôn dắt tôi đi cùng đường ngõ xóm chỉ để em được vui. Có cả những trận cãi nhau nảy lửa của ba anh em nữa…',NULL,5,'2014-02-20',NULL),(5,'Đất nước','Đất nước',5,'Đã bao nhiêu lần tôi tự hỏi: liệu trên Thế giới này có nơi nào chiến tranh tang thương mà lại rất đổi anh hùng như nước mình không? Liệu có mảnh đất nào mà mỗi tấc đất hôm nay đã thấm máu xương của những thế hệ đi trước nhiều như nước mình không? Và, liệu có một đất nước nào lại có nhiều bà mẹ đau khổ nhưng cũng hết sức gan góc như đất nước mình không?',NULL,1,'2010-05-25',NULL),(6,'Hard Rock Hallelujah','Hard Rock Hallelujah',7,'Những linh hồn đang lạc lối, mù quáng mất phương hướng trong cõi trần gian đầy nghiệt ngã hãy nên lắng nghe \"Hard Rock Hallelujah\" để có thể quên tất cả mọi thứ để tìm về đúng bản chất sâu thẳm nhất trong tâm hồn chính mình!',NULL,6,'2013-09-12',NULL),(7,'The Unforgiven','The Unforgiven',7,'Lâu lắm rồi mới nghe lại The Unforgiven II, vì bài này không phải là bài mà tôi thích. Anh bạn tôi lúc trước, đi đâu cũng nghêu ngao bài này ấy, chỉ tại vì hắn đang... thất tình mà lị. Mà sao Metallica có The Unforgiven rồi lại có thêm bài này chi nữa vậy không biết nữa, làm cho tôi cảm thấy hình như hơi bị đúng so với tâm trạng của tôi lúc này.',NULL,1,'2010-05-25',NULL),(8,'Nơi tình yêu bắt đầu','Nơi tình yêu bắt đầu',1,'Nhiều người sẽ nghĩ làm gì có yêu nhất và làm gì có yêu mãi. Ừ! Chẳng có gì là mãi mãi cả, vì chúng ta không trường tồn vĩnh cửu',NULL,1,'2014-02-03',NULL),(9,'Love Me Like There’s No Tomorrow','Love Me Like There’s No Tomorrow',8,'Nếu ai đã từng yêu Queen, yêu cái chất giọng cao, sắc sảo như một vết cắt thật ngọt ẩn giấu bao cảm xúc mãnh liệt của Freddie chắc không thể không \"điêu đứng\" mỗi khi nghe Love Me Like There’s No Tomorrow.',NULL,1,'2013-02-26',NULL),(10,'I\'m stronger','I\'m stronger',7,'Em không phải là người giỏi giấu cảm xúc, nhưng em lại là người giỏi đoán biết cảm xúc của người khác vậy nên đừng cố nói nhớ em, rằng mọi thứ chỉ là do hoàn cảnh. Và cũng đừng dối em rằng anh đã từng yêu em. Em nhắm mắt cũng cảm nhận được mà, thật đấy',NULL,2,'2013-08-21',NULL),(11,'Ôi Cuộc Sống Mến Thương','Ôi Cuộc Sống Mến Thương',5,'Có một câu nói như thế này \"Âm nhạc là một cái gì khác lạ mà hầu như tôi muốn nói nó là một phép thần diệu.Vì nó đứng giữa tư tưởng và hiện tượng, tinh thần và vật chất, mọi thứ trung gian mơ hồ thế đó mà không là thế đó giữa các sự vật mà âm nhạc hòa giải\"',NULL,2,'2011-10-09',NULL),(12,'Cây và gió','Cây và gió',7,'Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.',NULL,7,'2013-12-05',NULL);
+/*!40000 ALTER TABLE `baiviet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tacgia`
+--
+
+DROP TABLE IF EXISTS `tacgia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tacgia` (
+  `ma_tgia` int NOT NULL DEFAULT (NULL),
+  `ten_tgia` varchar(100) NOT NULL DEFAULT (NULL),
+  `hinh` longblob DEFAULT (NULL),
+  PRIMARY KEY (`ma_tgia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tacgia`
+--
+
+LOCK TABLES `tacgia` WRITE;
+/*!40000 ALTER TABLE `tacgia` DISABLE KEYS */;
+INSERT INTO `tacgia` VALUES (1,'Nhacvietplus',NULL),(2,'Sưu tầm',NULL),(3,'Sandy',NULL),(4,'Lê Trung Ngân',NULL),(5,'Khánh Ngọc',NULL),(6,'Night Stalker',NULL),(7,'Phạm Phương Anh',NULL);
+/*!40000 ALTER TABLE `tacgia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `theloai`
+--
+
+DROP TABLE IF EXISTS `theloai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `theloai` (
+  `ma_tloai` int unsigned NOT NULL DEFAULT (NULL),
+  `ten_tloai` varchar(30) NOT NULL DEFAULT (NULL),
+  PRIMARY KEY (`ma_tloai`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `theloai`
+--
+
+LOCK TABLES `theloai` WRITE;
+/*!40000 ALTER TABLE `theloai` DISABLE KEYS */;
+INSERT INTO `theloai` VALUES (1,'Nhạc trẻ'),(2,'Nhạc trữ tình'),(3,'Nhạc cách mạng'),(4,'Nhạc thiếu nhi'),(5,'Nhạc quê hương'),(6,'POP'),(7,'Rock'),(8,'R&B');
+/*!40000 ALTER TABLE `theloai` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-12 18:30:10
