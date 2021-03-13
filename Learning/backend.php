ecc1a4fac466026a015c0d8e4cc7790f8c3ee0bf
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-13 10:59:26 +0700

    no message

--- Learning/backend.php
+++ Learning/backend.php
@@ -3,3 +3,13 @@
         echo "<h1>Done</h1>";
     } else echo ("<h1>No data</h1>");
 ?>
+
+<html>
+    <body>
+        <form action="backend.php" method="POST">
+            <p>Name: <input type="text" name="name"></p>
+            <p>Age: <input type="text" name="age"></p>
+            <input type="submit" value="Send" id="btn_submit">
+        </form>
+    </body>
+</html>
